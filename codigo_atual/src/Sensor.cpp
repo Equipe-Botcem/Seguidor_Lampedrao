bea882ed01ab324e191fea52314b47fbe40037c7
PHM-araujo <pedromeloaraujo1999@gmail.com>
2022-11-05 20:10:10 -0300

    Refatorada classe do sensor para calibracao

--- codigo_atual/src/Sensor.cpp
+++ codigo_atual/src/Sensor.cpp
@@ -34,7 +34,35 @@ void Sensor::Rotina()
 //! implementar
 void Sensor::Calibracao()
 {
+
+	
 }
+
+int Sensor::find_min(){
+	int v_lido=0;
+	int v_min=1023;
+	for(unsigned int i=0;i<20;i++){
+
+		v_lido = analogRead(pin_sensor);
+		if(v_lido<v_min) v_min = v_lido;
+	}
+	return v_min;
+}
+
+int Sensor::find_max(){
+	int v_lido=0;
+	int v_max=0;
+	for(unsigned int i=0;i<20;i++){
+
+		v_lido = analogRead(pin_sensor);
+		if(v_lido>v_max) v_max = v_lido;
+	}
+}
+
+void Sensor::setValorMed(double val_med){
+	calib_val_med = val_med;
+}
+
 //! implementar
 void Sensor_esq::Rotina()
 {
