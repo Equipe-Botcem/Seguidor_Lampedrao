73ed699e43188e7d42ff4f06142ca83b2699caad
PHM-araujo <pedromeloaraujo1999@gmail.com>
2022-11-05 21:53:03 -0300

    Comportamento da função main

--- codigo_atual/src/main.cpp
+++ codigo_atual/src/main.cpp
@@ -1,15 +1,19 @@
-
 #include "Seguidor/Seguidor.h"
+#include "Utilities/Utilities.h"
+
+
+Seguidor seguidor = Seguidor();
+String command = "";
 
 void setup()
 {
-
 	Serial.begin(115200);
+	seguidor.Init();
+	initBluetooth();
 }
 
 void loop(){
-
-	
+	seguidor.Behavior(comunica_serial());
 }
 
 
